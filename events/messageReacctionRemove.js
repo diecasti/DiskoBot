82e2ef784badd57fa55675fe83ad9d9950f504f2
diecasti <diecasti@ucm.es>
2021-11-07 12:49:58 +0100

    eventos message reaction

--- events/messageReacctionRemove.js
+++ events/messageReacctionRemove.js
@@ -1,5 +1,5 @@
 module.exports = {
-	name: 'messageReactionAdd',
+	name: 'messageReactionRemove',
 	async execute(reaction, user) {
 
         if (user.bot)
@@ -16,56 +16,48 @@ module.exports = {
             }
         }
 
-
         if (reaction.message.embeds.length >= 1){
             if (reaction.message.embeds[0].title === 'Role Table'){
                 if (reaction.emoji.name == '1️⃣'){
                     const mrole = reaction.message.guild.roles.cache.find(role => role.name === 'Miembro');
-                    await reaction.message.guild.members.cache.get(user.id).roles.add(mrole);
+                    await reaction.message.guild.members.cache.get(user.id).roles.remove(mrole);
                 }
                 else 
                 if (reaction.emoji.name == '2️⃣'){
                     const mrole = reaction.message.guild.roles.cache.find(role => role.name === 'Esport');
-                    await reaction.message.guild.members.cache.get(user.id).roles.add(mrole);
+                    await reaction.message.guild.members.cache.get(user.id).roles.remove(mrole);
                 }
                 else 
                 if (reaction.emoji.name == '3️⃣'){
                     const mrole = reaction.message.guild.roles.cache.find(role => role.name === 'Valorant');
-                    await reaction.message.guild.members.cache.get(user.id).roles.add(mrole);
+                    await reaction.message.guild.members.cache.get(user.id).roles.remove(mrole);
                 }
                 else 
                 if (reaction.emoji.name == '4️⃣'){
                     const mrole = reaction.message.guild.roles.cache.find(role => role.name === 'Cs go');
-                    await reaction.message.guild.members.cache.get(user.id).roles.add(mrole);
+                    await reaction.message.guild.members.cache.get(user.id).roles.remove(mrole);
                 }
                 else 
                 if (reaction.emoji.name == '5️⃣'){
                     const mrole = reaction.message.guild.roles.cache.find(role => role.name === 'Warzone');
-                    await reaction.message.guild.members.cache.get(user.id).roles.add(mrole);
+                    await reaction.message.guild.members.cache.get(user.id).roles.remove(mrole);
                 }
                 else 
                 if (reaction.emoji.name == '6️⃣'){
                     const mrole = reaction.message.guild.roles.cache.find(role => role.name === 'TFT');
-                    await reaction.message.guild.members.cache.get(user.id).roles.add(mrole);
+                    await reaction.message.guild.members.cache.get(user.id).roles.remove(mrole);
                 }
                 else 
                 if (reaction.emoji.name == '7️⃣'){
                     const mrole = reaction.message.guild.roles.cache.find(role => role.name === 'Rocket League');
-                    await reaction.message.guild.members.cache.get(user.id).roles.add(mrole);
+                    await reaction.message.guild.members.cache.get(user.id).roles.remove(mrole);
                 }
                 else 
                 if (reaction.emoji.name == '8️⃣'){
                     const mrole = reaction.message.guild.roles.cache.find(role => role.name === 'Evento Esport');
-                    await reaction.message.guild.members.cache.get(user.id).roles.add(mrole);
+                    await reaction.message.guild.members.cache.get(user.id).roles.remove(mrole);
                 }
             }
         }
-    
-        // // Now the message has been cached and is fully available
-        // console.log(`${reaction.message.author}'s message "${reaction.message.embeds.length}" gained a reaction!`);
-        // // The reaction is now also fully available and the properties will be reflected accurately:
-        // console.log(`${reaction.count} user(s) have given the same reaction to this message!`);
-    
-
     },
 };
